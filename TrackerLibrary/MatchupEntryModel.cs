8babfce502685a90a11573eb7506733e9a46ff3b
mario <mmartinez@thelabconsulting.com>
2021-06-16 09:39:00 -0500

    Add project files.

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+	public class MatchupEntryModel
+	{
+		/// <summary>
+		/// Represents one team in matchup
+		/// </summary>
+		public TeamModel TeamCompeting { get; set; }
+		/// <summary>
+		/// Represents score for this particular team.
+		/// </summary>
+		public double Score { get; set; }
+		/// <summary>
+		/// Represent matchup which this team camre from
+		/// as the winner.
+		/// </summary>
+		public MatchupModel ParentMatchup { get; set; }
+
+
+	}
+}
---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+		public List<PersonModel1> TeamMembers { get; set; } = new List<PersonModel1>();
+		public string TeamName { get; set; }
+	}
+}
